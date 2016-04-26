a0c758d664eb3b8df1c9b2f96eed9fa04d724636
afrolionx <mike@mikecorp.ca>
2016-04-26 01:03:26 -0400

    added addItem component to Home component

--- src/app.js
+++ src/app.js
@@ -6,22 +6,27 @@ const Value = require('nhg/value');
 
 // Components
 const Home = require('./components/home');
+const AddItem = require('./components/add-item');
 
 module.exports = App;
 
 function App () {
   let state = State({
     title: Value('Simple Mercury App'),
-    home: Home()
+    home: Home(),
+    addItem: AddItem()
   });
 
   return state;
 }
 
 App.render = function render (state) {
+  let appState = JSON.stringify(state);
+
   return h('div', [
     h('h1', state.title),
-    h('span', 'App.render(state): ' + JSON.stringify(state)),
-    Home.render(state.home)
+    h('span', 'App.render(state): ' + appState),
+    Home.render(state.home),
+    AddItem.render(state.addItem)
   ]);
 };
